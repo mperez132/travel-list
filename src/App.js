62bdee137a42f08f75ad9d5b4795cece20372f83
Moises Perez <perezmoises132@gmail.com>
2023-08-28 13:04:24 -0600

    Added basics for App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 export default function App() {
-  function Logo() {
-    return <h1>🌴 Far Away 💼</h1>;
-  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
 
-  function Form() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip? 🤔</h3>
+    </div>
+  );
+}
 
-  function PackingList() {}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
 
-  function Stats() {}
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have X items on your list, and you alread packed X (X%)</em>
+    </footer>
+  );
 }
