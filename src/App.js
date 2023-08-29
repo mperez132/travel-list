9e397c5c37093f41e4901bf0826cfb1af1642fbc
Moises Perez <perezmoises132@gmail.com>
2023-08-29 12:52:27 -0600

    strike-through for packed items.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
   { id: 3, description: "Charger", quantity: 2, packed: false },
   { id: 4, description: "Hat", quantity: 1, packed: false },
 ];
@@ -42,7 +42,7 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li>
-      <span>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
       <button>❌</button>
