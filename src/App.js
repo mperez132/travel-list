bc6b42f37391c51981e85e1324e235f92d6e4752
Moises Perez <perezmoises132@gmail.com>
2023-08-30 12:06:50 -0600

    Added setQuantity for Item.

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(5);
   function handleSubmit(e) {
     e.preventDefault();
   }
@@ -30,7 +31,10 @@ function Form() {
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 🤔</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
