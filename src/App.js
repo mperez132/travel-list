1e24d4a081d431e3e52bc3c50a2cc45466dcb16e
Moises Perez <perezmoises132@gmail.com>
2023-09-05 09:40:48 -0600

    Updates to form and newItem

--- src/App.js
+++ src/App.js
@@ -23,9 +23,14 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(5);
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
